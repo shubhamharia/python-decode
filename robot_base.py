f5423548f2e5cacb3aca810d9ce14d7ad1ac384e
shubhamharia <60798375+shubhamharia@users.noreply.github.com>
2020-04-14 06:00:58 +0100

    Update robot_base.py

--- robot_base.py
+++ robot_base.py
@@ -1,14 +1,20 @@
 import serial
+import time
 ser = serial.Serial("COM3",9600)
 while 1:
- val=b'(input("enter F To move forward or B to move backward or R to turn right or L to turn left or S to stop"))'
- if val == 'F','B','L','R':
-  ser.write(val)
-  print (val)
-  a = ser.read()
- 
+ time.sleep(2)
+ val=int(input("enter 1 To move forward or 2 to move backward or 3 to turn right or 4 to turn left or 5 to stop"))
+ if val<=5:
+  if val ==1:
+     ser.write(b'A')
+  if val ==2:
+   ser.write(b'B')
+  if val ==3:
+   ser.write(b'C')
+  if val ==4:
+   ser.write(b'D') 
+  if val ==5:
+   ser.close() 
+   
  else:
-  if val == 'S':
-   ser.close()
-  else:
    print('enter a velid number')
